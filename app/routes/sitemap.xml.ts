2af9ec3549515a1416736b878ed337d903cd2540
qlitre <kuri0403@gmail.com>
2025-10-06 22:46:42 +0900

    add sitemap.xml

---
+++ app/routes/sitemap.xml.ts
@@ -0,0 +1,33 @@
+import { createRoute } from 'honox/factory'
+import { config } from '../siteSettings'
+import type { Visit } from '../types/microcms'
+import { jstDatetime } from '../utils/jstDatetime'
+import { getMicroCMSClient } from '../libs/microcms'
+
+export default createRoute(async (c) => {
+  const client = getMicroCMSClient({
+    serviceDomain: c.env.SERVICE_DOMAIN,
+    apiKey: c.env.API_KEY,
+  })
+  const vistis = await client.getAllContents<Visit>({
+    endpoint: 'visits',
+    queries: { orders: '-visit_date' },
+  })
+  const urls = []
+  for (const visit of vistis) {
+    const jst = jstDatetime(visit.updatedAt).split('T')[0]
+    urls.push(`
+      <url>
+        <loc>${config.siteurl}/visits/${visit.id}</loc>
+        <lastmod>${jst}</lastmod>
+      </url>`)
+  }
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${config.siteurl}/</loc>
+    </url>
+    ${urls.join('')}
+  </urlset>`
+  return c.text(sitemap, 200, { 'Content-Type': 'application/xml' })
+})
---
+++ app/siteSettings.ts
@@ -0,0 +1,3 @@
+export const config={
+    siteurl:'https://meshi-log.info'
+}
