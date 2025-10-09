ce8c55417eada14dbf6fb264486ba470ae52521e
qlitre <kuri0403@gmail.com>
2025-10-09 10:04:20 +0900

    update sitemap.xml

--- app/routes/sitemap.xml.ts
+++ app/routes/sitemap.xml.ts
@@ -17,17 +17,22 @@ export default createRoute(async (c) => {
   for (const visit of allVisits) {
     const jst = jstDatetime(visit.updatedAt).split('T')[0]
     urls.push(`
-      <url>
-        <loc>${config.siteurl}/visits/${visit.id}</loc>
-        <lastmod>${jst}</lastmod>
-      </url>`)
+  <url>
+    <loc>${config.siteurl}/visits/${visit.id}</loc>
+    <lastmod>${jst}</lastmod>
+    <priority>0.8</priority>
+  </url>`)
   }
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${config.siteurl}/</loc>
-    </url>
-    ${urls.join('')}
-  </urlset>`
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <url>
+    <loc>${config.siteurl}/</loc>
+    <lastmod>${jstDatetime(new Date().toISOString()).split('T')[0]}</lastmod>
+    <priority>1.0</priority>
+  </url>${urls.join('')}
+</urlset>`
   return c.text(sitemap, 200, { 'Content-Type': 'application/xml' })
 })
